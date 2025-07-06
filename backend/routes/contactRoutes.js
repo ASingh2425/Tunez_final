e51045d68b45b58badd242423bdc8499d60ade6c
Anvesha <9a.anveshasingh@gmail.com>
2025-07-06 15:51:00 +0530

    Delete backend directory

--- backend/routes/contactRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { postContact, getContacts } from "../controllers/contactController.js";
-
-const router = express.Router();
-
-router.post("/", postContact);
-router.get("/", getContacts);
-
-export default router;
