50f203dd7961bf90847e1d3b9c9fc4b2b9e4cc96
Anvesha <9a.anveshasingh@gmail.com>
2025-07-06 10:36:35 +0530

    Delete frontend/app/components directory

--- frontend/app/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <div className="bg-black text-white text-center py-4">
-      <p className="text-sm">&copy; 2025 Tunez Music by Ruchi Pawar and Anvesha Singh. All rights reserved.</p>
-    </div>
-  );
-}
-
-export default Footer;
